a2b93bc2f6e096b4ebf1ecdcf418249a392e17d8
Abdul Ghafoor <agsghafoor601@gmail.com>
2026-06-23 16:16:23 +0500

    Secrets update

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(express.static(__dirname)); // Serve static frontend files
 const pool = mysql.createPool({
     host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || 'khangamer9',
+    password: process.env.DB_PASSWORD || '',
     database: process.env.DB_NAME || 'unigateway_db',
     waitForConnections: true,
     connectionLimit: 10,
--- setup-db.js
+++ setup-db.js
@@ -7,7 +7,7 @@ async function setupDatabase() {
         const connection = await mysql.createConnection({
             host: process.env.DB_HOST || 'localhost',
             user: process.env.DB_USER || 'root',
-            password: process.env.DB_PASSWORD || 'ghafoor'
+            password: process.env.DB_PASSWORD || ''
         });
 
         console.log("Connected to MySQL server.");
